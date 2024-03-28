c3eb5d19a31f9e9d8510f50205c2557d20b3764e
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-03-28 18:43:53 +0900

    [Silver III] Title: 파일 정리, Time: 212 ms, Memory: 43604 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,10 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
 
 file_list = {}
 
 for _ in range(N):
-    _, extension = input().split('.')
+    _, extension = input().rstrip().split('.')
     file_list[extension] = file_list.get(extension, 0) + 1
-        
-for key,value in sorted(file_list.items()):
-    print(f"{key} {value}")
+
+for key, value in sorted(file_list.items()):
+    print(f'{key} {value}')
