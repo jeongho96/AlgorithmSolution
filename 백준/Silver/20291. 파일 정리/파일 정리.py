1bba1f2419fd004c65fd4139be41d57247e095ec
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-03-28 15:35:23 +0900

    [Silver III] Title: 파일 정리, Time: 2020 ms, Memory: 43604 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -2,12 +2,9 @@ N = int(input())
 
 file_list = {}
 
-for i in range(N):
-    name, extension = map(str, input().split('.'))
-    if file_list.get(extension) is None:
-        file_list[extension] = 1
-    else:
-        file_list[extension] += 1
+for _ in range(N):
+    _, extension = input().split('.')
+    file_list[extension] = file_list.get(extension, 0) + 1
         
 for key,value in sorted(file_list.items()):
     print(f"{key} {value}")
