72805b79e00be679dba35e7143deff458557ae61
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-03-29 10:07:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 620 ms, Memory: 141644 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -10,13 +10,6 @@ sang_counter = Counter(sang_card)
 
 M = int(input().rstrip())
 check_card = list(map(int, input().rstrip().split()))
-answer_list = []
-
-for i in range(M):
-    if sang_counter[check_card[i]] == 0:
-        answer_list.append(0)
-    else:
-        answer_list.append(sang_counter[check_card[i]])
+answer_list = [sang_counter[card] for card in check_card]
 
 print(" ".join(map(str, answer_list)))
-
