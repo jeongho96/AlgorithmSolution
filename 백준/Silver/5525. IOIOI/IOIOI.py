c5d5e81e83bfef5e58a8582151ed4837300e622b
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-03-30 16:37:53 +0900

    [Silver I] Title: IOIOI, Time: 380 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,12 +6,18 @@ N = int(input())
 M = int(input())
 S = input().rstrip()
 
-check_string = 'O'.join(('I' * (N + 1)))
+answer = 0
 count = 0
 
-for i in range(len(S)):
-    if S[i:i+len(check_string)] == check_string:
-        if i+len(check_string) <= len(S):
-            count += 1
-
-print(count)
+i = 0
+while i <= (M-2):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count += 1
+        if count == N:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+print(answer)
