1a858d61c8069aa6beb9db10d28b5c6913727c66
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-04-03 16:36:39 +0900

    [Gold V] Title: 탑, Time: 548 ms, Memory: 161200 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,17 +1,22 @@
-n = int(input())
-tower_heights = list(map(int, input().split()))
-
+# 입력 받기: 탑의 개수 및 각 탑의 높이
+N = int(input())
+towers = list(map(int, input().split()))
+# 스택 초기화 및 정답을 저장할 리스트 초기화
 stack = []
-result = []
+answer = [0] * N
 
-for i in range(n):
-    while stack:
-        if stack[-1][1] > tower_heights[i]:
-            result.append(stack[-1][0] + 1)
-            break
+# 모든 탑을 순회하면서
+for i in range(N):
+    # 현재 스택이 비어있지 않고, 스택의 맨 위에 있는 탑의 높이가 현재 탑의 높이보다 작은 경우
+    while stack and stack[-1][1] < towers[i]:
+        # 스택에서 해당 탑을 제거
         stack.pop()
-    if not stack:
-        result.append(0)
-    stack.append((i, tower_heights[i]))
+    # 스택이 비어있지 않다면(즉, 현재 탑보다 높이가 큰 탑이 있다면)
+    if stack:
+        # 정답 리스트에 스택의 맨 위에 있는 탑의 인덱스 + 1을 저장
+        answer[i] = stack[-1][0] + 1
+    # 현재 탑을 스택에 추가
+    stack.append((i, towers[i]))
 
-print(*result)
+# 정답 출력
+print(' '.join(map(str, answer)))
