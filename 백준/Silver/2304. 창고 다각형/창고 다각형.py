ade979b74fa3106110ecc6cf1d4dd177942b8d05
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-04-06 17:10:34 +0900

    [Silver II] Title: 창고 다각형, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,45 +1,63 @@
-# 기둥 개수
-N = int(input())
-# 가장 높은 길이와 인덱스, 가장 끝에 있는 번호와 그 때의 높이를 구해준다.
-pillar = []
-highest = 0
-longest = 0
-highest_idx = 0
-longest_height =0
-for i in range(N):
-    a,b = map(int, input().split())
-    # 왼쪽 면과 높이를 각각 리스트에 넣어준다.
-    pillar.append((a,b))
-    if b > highest:
-        # 가장 높을 때의 인덱스
-        highest = b
-        highest_idx = a
-    if a > longest:
-        # 가장 끝에 있는 기둥 번호와 높이
-        longest = a
-        longest_height = b
+import sys
 
-# 가장 높은 곳을 기준으로 좌측, 우측으로 나눠서 해준다.
-sum = 0
-# 높이 기록해줄 배열
-arr=[0]*(longest+1)
-nopi = 0
-# 각 번호에 맞는 높이 기록
-for i in range(len(pillar)):
-    arr[pillar[i][0]] = pillar[i][1]
+n = int(sys.stdin.readline())
+storage_list = [0] * 1001
+max_h = 0 # 최대 높이
+max_h_l = 0 # 최대 높이의 위치
+end_idx = 0 # 마지막 위치
 
-# 가장 높은 곳 기준 좌측
-for j in range(1,highest_idx):
-    # 다음 높이가 이전 높이보다 낮아면, 이전 높이로 통일
-    if not arr[j] > arr[j-1]:
-        arr[j] = arr[j-1]
-# 우측
-for k in range(longest, highest_idx, -1):
-    # 왼쪽 높이가 오른쪽보다 낮아면 오른쪽높이로 통일
-    if not arr[k-1] > arr[k]:
-        arr[k-1] = arr[k]
+# 반복문을 통해 최대 높이를 구한다.
+for _ in range(n):
+    l, h = map(int, input().split())
+    storage_list[l] = h # l 위치에 h 높이의 박스
 
-for l in arr:
-    sum += l
+    # 현재 높이가 최대 높이보다 크다면
+    if max_h < h:
+        max_h = h # 최대 높이 초기화
+        max_h_l = l # 최대 높이의 위치 초기화
 
-print(sum)
+    # 마지막 인덱스 확인
+    end_idx = max(end_idx, l)
+
+answer = 0
+stack = []
+
+# 왼쪽부터 벽을 확인
+# 반복문에 조건을 보게되면
+# 최대 높이의 위치 + 1까지 반복하여 최대 높이까지 더해주는 것을 확인할 수 있다.
+for i in range(0, max_h_l+1):
+
+    # 저장된 상자가 없으면
+    if not stack:
+        # 현재 위치에 상자를 추가
+        stack.append(storage_list[i])
+        # 현재 위치에 상자를 창고 넓이에 더한다.
+        answer += stack[-1]
+
+    # 저장된 상자가 있으면
+    else:
+        # 창고에 마지막 상자의 높이와 현재 위치에 상자에 높이를 비교
+        if stack[-1] < storage_list[i]:
+            # 현재 위치에 상자에 높이가 더 높다면
+            # 창고에 마지막 상자의 높이를 현재 상자의 높이로 초기화
+            stack.pop()
+            stack.append(storage_list[i])
+
+        # 창고의 넓이에 창고에 마지막 상자의 높이를 더한다.
+        answer += stack[-1]
+
+stack = []
+# 오른쪽부터 벽을 확인
+# 왼쪽부터 확인하는 것과 같고
+# 다른점은 최대 높이의 벽을 창고의 넓이에 더하지 않는다는 것이다.
+for i in range(end_idx, max_h_l, -1):
+    if not stack:
+        stack.append(storage_list[i])
+        answer += stack[-1]
+    else:
+        if stack[-1] < storage_list[i]:
+            stack.pop()
+            stack.append(storage_list[i])
+        answer += stack[-1]
+
+print(answer)
