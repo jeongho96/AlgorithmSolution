b7cf55c2f0b598b009f0f35908be5d4e661e8bc8
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-04-09 17:25:53 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 4708 ms, Memory: 152728 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -1,27 +1,23 @@
-from sys import stdin
+import sys
 from heapq import *
 
-input = stdin.readline
-
+input = sys.stdin.readline
 
 T = int(input().rstrip())
 result = []
+
 for t in range(T):
     N = int(input().rstrip())
     chapter_heap = list(map(int, input().rstrip().split()))
     heapify(chapter_heap)
     total = 0
-    # 2개 미만이 될때까지
+
+    # 파일이 1개 이하일 때까지 합치기
     while len(chapter_heap) > 1:
-        # 가장 작은 두 파일 선택
-        first = heappop(chapter_heap)
-        second = heappop(chapter_heap)
-
-        # 선택된 두 파일 합치기
-        merged_file = first + second
+        # 가장 작은 두 파일 선택하여 합치기
+        merged_file = heappop(chapter_heap) + heappop(chapter_heap)
         total += merged_file
-
-        # 합친 파일을 다시 큐에 추가
+        # 합친 파일을 다시 최소 힙에 추가
         heappush(chapter_heap, merged_file)
 
     result.append(total)
