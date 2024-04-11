873ab51912b0cd7328818b0693323e41747ff9e1
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-04-11 12:47:39 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3380 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,65 +1,66 @@
-R, C, T = map(int, input().split())  # R: 행의 수, C: 열의 수, T: 시뮬레이션 시간
-A = [list(map(int, input().split())) for _ in range(R)]
+from sys import stdin
 
-# 공기청정기의 위치를 찾습니다.
-air_cleaner = []
-for i in range(R):
-    if A[i][0] == -1:
-        air_cleaner.append(i)
+input = stdin.readline
 
-dx = [-1, 1, 0, 0]  # 상하좌우 이동을 위한 dx, dy 정의
-dy = [0, 0, -1, 1]
 
-# 미세먼지 확산 함수
 def spread():
-    temp = [[0] * C for _ in range(R)]  # 확산된 후의 미세먼지 상태를 저장할 임시 배열
-    for x in range(R):
-        for y in range(C):
-            if A[x][y] > 0:
-                spread_amount = A[x][y] // 5  # 확산되는 미세먼지 양
-                spread_count = 0
-                for direction in range(4):
-                    nx, ny = x + dx[direction], y + dy[direction]
-                    if 0 <= nx < R and 0 <= ny < C and A[nx][ny] != -1:
-                        temp[nx][ny] += spread_amount
-                        spread_count += 1
-                A[x][y] -= spread_amount * spread_count
-    # 확산된 미세먼지를 원래 배열에 추가합니다.
-    for i in range(R):
-        for j in range(C):
-            A[i][j] += temp[i][j]
+    """
+    미세 먼지 확산 결과 반환 함수
+    """
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    new_board = [[0] * c for _ in range(r)]
+    new_board[ccw][0] = -1
+    new_board[cw][0] = -1
+    for x in range(r):
+        for y in range(c):
+            if board[x][y] > 0:
+                new_board[x][y] += board[x][y]
+                for d in range(4):
+                    nx = x + dx[d]
+                    ny = y + dy[d]
+                    if 0 <= nx < r and 0 <= ny < c and board[nx][ny] != -1:
+                        new_board[nx][ny] += board[x][y] // 5
+                        new_board[x][y] -= board[x][y] // 5
+    return new_board
 
-# 공기청정기 작동 함수
-def clean():
-    upper, lower = air_cleaner
 
-    # 위쪽 공기청정기 작동
-    for i in range(upper - 1, 0, -1):
-        A[i][0] = A[i - 1][0]
-    for i in range(C - 1):
-        A[0][i] = A[0][i + 1]
-    for i in range(upper):
-        A[i][C - 1] = A[i + 1][C - 1]
-    for i in range(C - 1, 1, -1):
-        A[upper][i] = A[upper][i - 1]
-    A[upper][1] = 0  # 공기청정기 바로 옆은 항상 0
+def rotate():
+    """
+    미세 먼지 순환 함수
+    """
+    # 위쪽 순환: 반시계 방향
+    for x in range(ccw - 1, 0, -1):
+        board[x][0] = board[x - 1][0]
+    for y in range(c - 1):
+        board[0][y] = board[0][y + 1]
+    for x in range(ccw):
+        board[x][-1] = board[x + 1][-1]
+    for y in range(c - 1, 0, -1):
+        board[ccw][y] = board[ccw][y - 1]
 
-    # 아래쪽 공기청정기 작동
-    for i in range(lower + 1, R - 1):
-        A[i][0] = A[i + 1][0]
-    for i in range(C - 1):
-        A[R - 1][i] = A[R - 1][i + 1]
-    for i in range(R - 1, lower, -1):
-        A[i][C - 1] = A[i - 1][C - 1]
-    for i in range(C - 1, 1, -1):
-        A[lower][i] = A[lower][i - 1]
-    A[lower][1] = 0  # 공기청정기 바로 옆은 항상 0
+    # 아래쪽 순환: 시계 방향
+    for x in range(cw + 1, r - 1):
+        board[x][0] = board[x + 1][0]
+    for y in range(c - 1):
+        board[-1][y] = board[-1][y + 1]
+    for x in range(r - 1, cw, -1):
+        board[x][-1] = board[x - 1][-1]
+    for y in range(c - 1, 0, -1):
+        board[cw][y] = board[cw][y - 1]
 
-# T번 시뮬레이션 실행
-for _ in range(T):
-    spread()  # 미세먼지 확산
-    clean()  # 공기청정기 작동
+    # 공기청정기에서 나온 바람은 미세 먼지가 없는 바람이므로 0으로 초기화
+    board[ccw][1] = 0
+    board[cw][1] = 0
 
-# 결과 출력
-result = sum(sum(row) for row in A) + 2  # 공기청정기 위치를 -1로 표시했으므로, 마지막에 2를 더해줍니다.
-print(result)
+
+r, c, t = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(r)]
+for i in range(r):
+    if board[i][0] == -1:
+        ccw, cw = i, i + 1
+        break
+for _ in range(t):
+    board = spread()
+    rotate()
+print(sum([sum(line) for line in board]) + 2) # 공기청정기 -1 * 2 고려
