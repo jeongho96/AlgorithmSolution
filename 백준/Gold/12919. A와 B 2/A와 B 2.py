7de45e0a03672714a3815bd8aea384e2a3cecaa7
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-04-15 15:23:16 +0900

    [Gold V] Title: A와 B 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+input = stdin.readline
+
+s = input().rstrip()
+t = input().rstrip()
+
+
+def dfs(t):
+    if len(s) == len(t):
+        if s == t:
+            print(1)
+            exit()
+        else:
+            return 0
+
+    if t[-1] == "A":
+        dfs(t[:-1])
+
+    if t[0] == "B":
+        dfs(t[::-1][:-1])
+
+
+dfs(t)
+print(0)
