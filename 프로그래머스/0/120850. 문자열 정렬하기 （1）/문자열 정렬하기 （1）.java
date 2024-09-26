b0261f44c942fe60dad40d2a28d63c3ceb4550a0
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-09-26 09:36:28 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.66 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        // 숫자가 아닌 경우에는 제거해서 문자열로 생성
+        String numberOnly = my_string.replaceAll("[^0-9]", "");
+
+        int[] answer = new int[numberOnly.length()];
+
+        for(int i =0; i < numberOnly.length(); i++) {
+            answer[i] = numberOnly.charAt(i) - '0';
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
