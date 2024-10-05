b809fd68e70e4b593ad1d12c8327a1b85784ed6a
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-10-05 19:15:12 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 388 ms, Memory: 21408 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static void solution(int N){
+
+        // 상단 부분
+        for (int i = 1; i <= N; i++) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 하단 부분
+        for (int i = N - 1; i >= 1; i--) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        solution(n);
+
+
+    }
+}
