02f43f7e80cc2f8a66f595457e090aaa8d8fbeb9
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-10-09 11:10:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -4,22 +4,6 @@ import java.util.List;
 
 class Solution {
     public int solution(int n) {
-        int answer = 2;
-        List<Integer> list = new ArrayList<Integer>();
-
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0){
-                list.add(i);
-            }
-        }
-
-        int sqrtNum = (int)Math.floor(Math.sqrt((double)n));
-
-        System.out.println(sqrtNum);
-
-        if(list.contains((int)Math.floor(Math.sqrt((double)n)))){
-            answer = 1;
-        }
-        return answer;
+        return (n % Math.sqrt(n) == 0 ? 1 : 2);
     }
 }
