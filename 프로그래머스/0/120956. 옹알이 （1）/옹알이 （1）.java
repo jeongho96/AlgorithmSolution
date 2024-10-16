03d7e2e07d7da40907855da81aa13ae11f29eaa2
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-10-16 10:18:10 +0900

    [level 0] Title: 옹알이 (1), Time: 1.42 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,34 +1,18 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-for(int i = 0; i < babbling.length; i++) {
-    int check = babbling[i].length();
-    for(int j = 0; j < babbling[i].length();) {
-        if(babbling[i].charAt(j) == 'a' && j + 3 <= babbling[i].length() && babbling[i].substring(j, j + 3).equals("aya")) {
-            check -= 3;
-            j += 3; // 인덱스를 3만큼 이동
-        } 
-        else if(babbling[i].charAt(j) == 'y' && j + 2 <= babbling[i].length() && babbling[i].substring(j, j + 2).equals("ye")) {
-            check -= 2;
-            j += 2; // 인덱스를 2만큼 이동
-        } 
-        else if(babbling[i].charAt(j) == 'm' && j + 2 <= babbling[i].length() && babbling[i].substring(j, j + 2).equals("ma")) {
-            check -= 2;
-            j += 2; // 인덱스를 2만큼 이동
-        } 
-        else if(babbling[i].charAt(j) == 'w' && j + 3 <= babbling[i].length() && babbling[i].substring(j, j + 3).equals("woo")) {
-            check -= 3;
-            j += 3; // 인덱스를 3만큼 이동
-        } 
-        else {
-            j++; // 아무 것도 안 맞으면 인덱스를 1만큼 이동
-        }
-    }
-    if(check == 0) {
-        answer++;
-    }
-}
+        for(int i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
 
-return answer;
+            if(babbling[i].isEmpty()) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
