2e2c86f17714722d55d938436bcb1a92360b3661
jeongho96 <47017708+jeongho96@users.noreply.github.com>
2024-12-11 11:56:32 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,25 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> clothesMap = new HashMap<String, List<String>>();
+        Map<String, Integer> clothesCount = new HashMap<>();
 
-        for(int i = 0; i < clothes.length; i++){
-            addClothing(clothesMap, clothes[i][1], clothes[i][0]);
+        for (String[] cloth : clothes) {
+            clothesCount.put(cloth[1], clothesCount.getOrDefault(cloth[1], 0) + 1);
         }
 
-        int answer = clothesMap.values().stream()
-                .mapToInt(list -> list.size() + 1)
-                .reduce(1, (a,b) -> a * b);
+        int answer = 1;
+        for (int count : clothesCount.values()) {
+            answer *= (count + 1); // 안 입는 경우 포함
+        }
 
-        return answer - 1;
-    }
-    
-    // 카테고리에 옷을 추가하는 메서드
-    private static void addClothing(Map<String, List<String>> map, String category, String item) {
-        // 해당 카테고리의 리스트가 없으면 새로 생성
-        map.putIfAbsent(category, new ArrayList<>());
-
-        // 카테고리에 아이템 추가
-        map.get(category).add(item);
+        return answer - 1; // 모두 안 입는 경우 제외
     }
 }
